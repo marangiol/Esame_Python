52045ff38bc4c689f4405e54bb639b0f6c3a8ad8
fbianconi20 <f.bianconi@unicampus.it>
2024-05-27 10:51:17 +0200

    Create Esercizio_anonimizza_v1.py

---
+++ Esercizio_anonimizza_v1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+24 - 05 - 2024
+
+@author: marianna
+"""
+import json
+
+file_path = 'anonimizza_test2.json'
+
+with open(file_path,'r') as file:
+    data=json.load(file)
+	
+	
+	
+# creo un dizionario vuoto 
+user_id={}
+
+#inizializzo contatore
+identific_unico=1
+
+for campo in data:
+	nome=campo[1]  
+	#estraggo il nome e lo aggiungo nel dizionario vuoto
+	if nome not in user_id: 
+		user_id[nome]=identific_unico
+		identific_unico += 1
+
+#sostitusco nel log l’identificatore unico al nome dell’utente
+
+for campo in data:       
+        nome= campo[1]
+        if nome in user_id:
+            campo[1]=user_id[nome]
+
+#elimino da tutti i log il campo “Utente coinvolto”
+for campo in data:       
+       campo.pop(2)
+
+#salvo la lista di log anonimizzata nello stesso formato di partenza (json)
+
+output ='anonimizza_13_output.json'
+with open(output,'w') as file:
+    json.dump(data,file, indent=4)  
+    
+ 
