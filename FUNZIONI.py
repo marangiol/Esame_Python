69e6491d7f7a2fc270840a74c5c4834c2aa7b094
dalilaucbm <160488143+dalilaucbm@users.noreply.github.com>
2024-05-27 11:32:01 +0200

    Update FUNZIONI.py

--- FUNZIONI.py
+++ FUNZIONI.py
@@ -11,3 +11,17 @@ def caricafile (file_path):
     with open(file_path,'r') as file:
         data=json.load(file)
     return data 
+
+def trovanomi (data):
+    """
+    Questa funzone mi aiuta ad associare ad ogni utente un numero 
+    """
+    user_id = {}
+
+    identific_unico = 1
+    for campo in data:       
+            nome= campo[1]
+            if nome not in user_id:
+                user_id[nome]=identific_unico
+                identific_unico += 1 
+    return user_id, identific_unico
